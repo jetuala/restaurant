111a8bb57dd8e16354639d12f93ce15ab19b9429
Jonathan Tuala <jetuala@gmail.com>
2022-10-17 22:47:17 -0700

    Structural comments

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -4,6 +4,10 @@ export default function Reservations() {
     return (
         <div>
             <h1>This is the Reservations page.</h1>
+            {/* Input for email address. */}
+            {/* Input for number of people, 2-8 */}
+            {/* Input for date and time in 15 minute increments */}
+            {/* Maybe if a time is not available, include a suggestion for the nearest time */}
         </div>
     )
 }
